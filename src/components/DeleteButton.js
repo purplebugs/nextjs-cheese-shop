1ffc2a58088d6f1c71652ccfbb6b3f8629e8a264
Anita Lipsky <anita.lipsky@noaignite.com>
2024-05-16 12:56:51 +0200

    refactor: Remove console logs

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,8 +1,6 @@
 "use client";
 
 async function deleteCheese(id) {
-  console.log(id);
-
   const res = await fetch(`/api/cheese/${id}`, { method: "DELETE" });
 
   if (!res.ok) {
@@ -11,7 +9,6 @@ async function deleteCheese(id) {
   }
 
   const result = await res.json();
-  console.log(result);
   alert(`
   Message from API: ${result?.message}.
 
